e618066ca2d9e558dd62727c9e2fd7217dc695bd
Glen Hinkle <glen@empireenterprises.com>
2011-04-17 04:07:17 -0400

    fixed footer, added fixture

--- t/fixtures/Blog.pl
+++ t/fixtures/Blog.pl
@@ -1,13 +1,25 @@
 [   Blog => {
-        id       => 1,
-        title    => 'Hello!',
-        subtitle => 'A subtitle',
+        id           => 1,
+        title        => 'Hello!',
+        subtitle     => 'A subtitle',
         created_time => '1302056856',
         location     => 'Chico, California',
 
         content =>
           q|I'm often introduced to new groups of people. Traveling, new contracts, and otherwise just being personable, present situations wherein people must deal with my presence.  I make an effort to treat everyone equally, no matter their social or professional position. I make an equal effort to execute appropriate proportions of irreverence; just enough to release the tension that tends to develop in groups of people. It develops because people are cesspools of fear.  That fear isn't always immediately apparent; it reveals itself, for the most part, in anger and unmet expectation. Sometimes, no, many times, I am victim to that fear. I can feel it bubbling inside me, coercing me to poise for an impending attack.|,
 
-        tags => [{id => 1, name => 'personal'}, {id => 2, name => 'test'}]
+        tags => [{name => 'personal'}, {name => 'test'}]
+    },
+    Blog => {
+        id           => 2,
+        title        => 'Tech',
+        subtitle     => 'A subtitle',
+        created_time => '1302056856',
+        location     => 'Chico, California',
+
+        content =>
+          q|I'm often introduced to new groups of people. Traveling, new contracts, and otherwise just being personable, present situations wherein people must deal with my presence.  I make an effort to treat everyone equally, no matter their social or professional position. I make an equal effort to execute appropriate proportions of irreverence; just enough to release the tension that tends to develop in groups of people. It develops because people are cesspools of fear.  That fear isn't always immediately apparent; it reveals itself, for the most part, in anger and unmet expectation. Sometimes, no, many times, I am victim to that fear. I can feel it bubbling inside me, coercing me to poise for an impending attack.|,
+
+        tags => [{name => 'tech'}],
     }
 ]
